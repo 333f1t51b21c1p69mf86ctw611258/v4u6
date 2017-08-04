3a1e161b479e4667662643f741460f65ca74ef66
Tran Huu Nguyen <nguyenth@dasannetworks.com>
2017-08-04 16:57:14 +0700

    Remove all default interface.

--- vhub.web/webpack/webpack.dev.js
+++ vhub.web/webpack/webpack.dev.js
@@ -79,8 +79,8 @@ module.exports = webpackMerge(commonConfig({ env: ENV }), {
             utils.root('src/test'),
         ]),
         new WebpackNotifierPlugin({
-            title: 'JHipster',
-            contentImage: path.join(__dirname, 'logo-jhipster.png')
+            title: 'Liferay',
+            contentImage: path.join(__dirname, 'liferay.png')
         })
     ]
 });
